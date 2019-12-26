413d5e40a71113627eea1aee25347ba7cb2206ca
Zchwazer <yamiyomuyumi@gmail.com>
2019-12-26 16:29:58 +0700

    Delete unuse file

--- functions/lib/route.js
+++ functions/lib/route.js
@@ -9,19 +9,19 @@ const router = require('express').Router();
 const userController = require('./userController');
 const stuController = require('./stuController');
 
-//# Use => https://us-central1-newagent-47c20.cloudfunctions.net/api/user
+//# GET => https://us-central1-newagent-47c20.cloudfunctions.net/api/user
 //~ Use for get all user data in web app
 router.get("/user" ,userController.getAllUser);
 
-//# Use => https://us-central1-newagent-47c20.cloudfunctions.net/api/user/{userId}
+//# GET => https://us-central1-newagent-47c20.cloudfunctions.net/api/user/{userId}
 //~ Use for get single user data in user mobile app or web app
 router.get("/user/:id" ,userController.getOnceUser);
 
-//# Use => {deploy url}
+//# POST => https://us-central1-newagent-47c20.cloudfunctions.net/api/user
 //~ Use for registration section in user mobile application
 router.use("/user" ,userController.addOnceUser);
 
-//# Use => {deploy url}
+//# Use => https://us-central1-newagent-47c20.cloudfunctions.net/api/user/{userId}
 //~ Use for admin change user level in web app
 // router.put("/user/:user", controller.updateOnceUser);
 
