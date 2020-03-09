580c2e0548cb0866e0161209d67e9b0c46bc172b
Zchwazer <yamiyomuyumi@gmail.com>
2020-03-10 00:32:41 +0700

    Create News notification topic

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,7 @@
+//! Initialize Firebase Admin to App
+const admin = require('firebase-admin');
+
+//---------------------------------------------------------------------//
 const functions = require('firebase-functions');
 const server = require('./src/server');
 const api = functions
@@ -8,8 +12,40 @@ const api = functions
     .https
     .onRequest(server);
 
+//---------------------------------------------------------------------//
+//! Cloud Trigger (Notification)
+//? News create Notification
+const sendNewsNotification = 
+    functions.firestore
+        .document("/news/{newsId}")
+        .onCreate((event) => {             
+            console.log("Hello World");
+            const news = event.data()
+
+            //! Notification Setting
+            const payload = {
+                //# Data display to app
+                notification:{
+                    title: 'ข่าวใหม่มาแล้วค่า',
+                    body: String(news.Topic),
+                },
+                
+                //# Data store in payload but not display
+                data:{
+                    Description: String(news.Description),
+                }
+            }
+            
+            return admin.messaging().sendToTopic("News",payload);
+        });   
+
+//---------------------------------------------------------------------//
+//! Send to Device
+
+//---------------------------------------------------------------------//
 module.exports = {
-    api
+    api,
+    sendNewsNotification
 };
 
 // // Create and Deploy Your First Cloud Functions
